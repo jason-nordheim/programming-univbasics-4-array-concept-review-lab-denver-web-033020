34944bc364d1cecf321ebab77d419df0e59f4ca4
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-02 17:22:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,24 +15,29 @@ def find_element_index(array, value_to_find)
   # implicitely return nil
 end
 
+# takes an array of integer values and returns the highest integer value
 def find_max_value(array)
   # default to first value
   max_value = array[0]
   do array.length | index |
     # if value at index is greater than the last
-    # highest values seen before
-    # replace the highest value 
+    # highest value seen before
+    # replace the highest value
     if max_value > array[index]
       max_value = array[index]
   end
   return max_value
 end
 
+# takes an array of integer values and returns the
 def find_min_value(array)
-  max_value = array[0]
+  min_value = array[0]
   do array.length | index |
-    if max_value < array[index]
-      max_value = array[index]
+    # if value at index is less than the last
+    # lowest value seen before
+    # replace the lowest value
+    if min_value < array[index]
+      min_value = array[index]
   end
   return max_value
 end
